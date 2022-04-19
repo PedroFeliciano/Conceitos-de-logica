9b5640791857f4dd2327cc569b9311006aeca0b9
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-04-19 19:21:11 -0300

    estudos3

--- dev/Calculator/Program.cs
+++ dev/Calculator/Program.cs
@@ -6,15 +6,49 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-         Console.Clear();
-         
-         Console.WriteLine("Primeiro Valor:");
-         float v1 =  float.Parse (Console.ReadLine());
+         Subtracao();
 
-         Console.ReadLine(v1);
         } 
         
-    }     
+
+        static void Soma(){
+        Console.Clear();
+         
+         Console.WriteLine("Primeiro Valor:");
+         float v1 =  float.Parse(Console.ReadLine());
+
+         
+         Console.WriteLine("Segundo Valor:");
+         float v2 =  float.Parse(Console.ReadLine());
+          
+
+         float soma = v1 + v2;
+         Console.WriteLine($"O resultado é {soma} "); 
+         
+         Console.ReadLine();
+         Console.ReadKey();
+
+        }
+    
+        static void Subtracao(){
+         
+        Console.Clear();
+
+        Console.WriteLine("Primeiro Valor");
+        float v1 = float.Parse(Console.ReadLine());
+          
+        Console.WriteLine("Segundo Valor");
+        float v2 = float.Parse(Console.ReadLine());  
+        
+        float subtraçao = v1-v2;
+        Console.WriteLine($"O Resultado  é {subtraçao}");
+        
+        Console.ReadKey();
+
+        }     
+    }  
+
+
         
           
 }      
