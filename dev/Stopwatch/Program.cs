7655ba662036c253da928226ea02f839ee4cf083
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-04-25 23:18:06 -0300

    estudos8

---
+++ dev/Stopwatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+
+            int time=10;
+            int currentTime =0; 
+            
+            while(currentTime != time )
+            {
+             Console.Clear();
+              currentTime ++;
+              Console.WriteLine(currentTime);
+             Thread.Sleep(1000);  
+            }
+            Console.WriteLine("O tempo acabou");
+        }
+    }
+}
