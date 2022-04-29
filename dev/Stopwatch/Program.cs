10d79c36fb2a6bf3c9149f596770c8821725a706
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-04-28 22:29:17 -0300

    estudos11

--- dev/Stopwatch/Program.cs
+++ dev/Stopwatch/Program.cs
@@ -21,9 +21,20 @@ namespace Stopwatch
 
       string data = Console.ReadLine().ToLower();
       char type = char.Parse(data.Substring(data.Length-1,1));
-      Console.WriteLine(data);
-      Console.WriteLine(type);     
+      int time = int.Parse(data.Substring(0,data.Length-1));
+      int multplier = 1;
+      //registra o ultimo caracter 
+
+    if(type=='m')
+       multplier = 60;    
+    
+    if (time==0)
+       System.Environment.Exit(0);
+       Start(time * multplier);
+
+    
      }
+
 
      
 
