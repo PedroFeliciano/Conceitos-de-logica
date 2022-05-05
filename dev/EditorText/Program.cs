00810487e1aee219e238c68b701a3bac8bfa4ec1
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-05-05 11:30:51 -0300

    estudos13

---
+++ dev/EditorText/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EditorText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Menu();
+        }
+    
+      static void Menu(){
+
+      Console.Clear();
+      
+      Console.WriteLine("O que deseja Fazer");
+      Console.WriteLine("");     
+      Console.WriteLine(" 1 ABRIR ARQUIVO");
+      Console.WriteLine("");
+      Console.WriteLine(" 2 CRIANDO NOVO ARQUIVO");
+      Console.WriteLine("");
+      Console.WriteLine(" 0 - SAIR");
+      Console.WriteLine("");
+      short option = short.Parse(Console.ReadLine());
+
+      
+      }
+
+
+    }
+}
