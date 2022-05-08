de211dd2450585fbb6870a3e75c95d75f9f37338
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-05-08 14:29:40 -0300

    estudos14

--- dev/EditorText/Program.cs
+++ dev/EditorText/Program.cs
@@ -13,7 +13,7 @@ namespace EditorText
 
       Console.Clear();
       
-      Console.WriteLine("O que deseja Fazer");
+      Console.WriteLine("O que deseja Fazer?");
       Console.WriteLine("");     
       Console.WriteLine(" 1 ABRIR ARQUIVO");
       Console.WriteLine("");
@@ -23,7 +23,19 @@ namespace EditorText
       Console.WriteLine("");
       short option = short.Parse(Console.ReadLine());
 
+       switch(option){
       
+      case 0:System.Environment.Exit(0); break;
+      case 1:Abrir();break;
+      case 2:editar();break;
+      default: Menu();break;
+       
+       }
+
+      static void Abrir() {}
+
+      static void editar() {}
+
       }
 
 
