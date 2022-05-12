db8f2d7570ffa280c24aada248e5c6f7adecbeae
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-05-12 08:54:12 -0300

    estudos17

--- dev/EditorText/Program.cs
+++ dev/EditorText/Program.cs
@@ -37,8 +37,22 @@ namespace EditorText
       }
       
 
-      static void Abrir(){}
-
+      static void Abrir(){
+    Console.Clear();
+    Console.WriteLine("Qual é o caminho do arquivo?");
+     string path = Console.ReadLine();
+     
+      using(var file = new StreamReader(path))
+      {
+         string text  =  file.ReadToEnd();
+         Console.WriteLine(text);
+      }
+      
+      Console.WriteLine("");
+      Console.ReadLine();
+      Menu();
+      
+      }
       static void Editar(){
      
      Console.Clear();
