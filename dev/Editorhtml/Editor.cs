d8dd10a94022a33731853238705f9ecd1abae630
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-06-02 11:40:04 -0300

    estudos23

---
+++ dev/Editorhtml/Editor.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+
+namespace EditorHml 
+{
+     public static class Editor
+     {
+         public static void Show() {
+         
+         Console.Clear();
+         Console.WriteLine("MODO EDITOR");
+         Console.WriteLine("-----------");
+         Start();
+
+         }
+     
+     public static void Start() {
+         var file = new StringBuilder();
+
+        do {
+            file.Append(Console.ReadLine);
+            file.Append(Environment.NewLine);
+        } while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.WriteLine("-------------");
+        Console.WriteLine("Deseja salvar o arquivo?");
+        
+
+     }
+     
+     }
+}
